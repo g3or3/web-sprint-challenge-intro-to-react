85086ef5015be122c361fffd0ee8f65018fef53e
g3or3 <>
2021-03-19 16:55:08 -0400

    added some more styles

--- src/components/Character.js
+++ src/components/Character.js
@@ -3,30 +3,38 @@ import React, {useState} from 'react';
 import styled from 'styled-components';
 
 const StyledCharacter = styled.div`
-    color: black;
+    color: #2e557c;
     border: solid 5px;
     background: white;
     margin: 2% auto;
-    width: 70%;
+    width: 75%;
     box-sizing: border-box;
 
     h1 {
-        margin: 1%;
+        margin: 3%;
         font-family: 'starjedi';
     }
 
     p {
-        margin: 2px;
-        padding: 1% 2%;
+        display: inline-block;
+        margin: 5px;
+    }
+
+    span {
+        font-weight: bold;
+    }
+
+    div {
+        padding-left: 2%;
         text-align: left;
-        display: block;
+        color: #443e3e;
     }
 `
 
 const Character = function({character}) {
     const [currCharacter, setCurrCharacter] = useState(null)
     
-    const fixedAttributes = ['name', 'height', 'mass', 'hair_color', 'skin_color', 'eye_color', 'birth_year', 'gender', 'films']
+    const fixedAttributes = ['name', 'height', 'mass', 'hair_color', 'skin_color', 'eye_color', 'birth_year', 'gender']
 
     function showDetails(character) {
         currCharacter === null ? setCurrCharacter(character) : setCurrCharacter(null)
@@ -36,7 +44,12 @@ const Character = function({character}) {
     <StyledCharacter onClick = {() => {showDetails(character)}}>
         <h1>{character["name"]}</h1>
         {currCharacter && fixedAttributes.map(attribute => {
-            return <p key = {attribute}>{`${attribute}: ${character[attribute]}`}</p>
+            return (
+                <div>
+                <span>{attribute}: </span>
+                <p key = {attribute}>{character[attribute]}</p>
+                </div>
+            )
         })}
     </StyledCharacter>
     )
